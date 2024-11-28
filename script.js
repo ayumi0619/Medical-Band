dcbfaed425cfc199aa3a3fa488049510b5ba97c0
ayumi0619 <108720099+ayumi0619@users.noreply.github.com>
2024-11-28 13:14:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
-// JavaScript
+$(function(){
 
-const main = document.querySelector('#main-image');
-const thumbs = document.querySelectorAll('.thumb');
 
-// 確認
-//console.log(main);
-//console.log(thumbs);
-thumbs.forEach(function(item, index) {
-    item.onclick = function() {
-      //console.log(this.dataset.image);
-    main.src = this.dataset.image;
-    }
+  });
+
+  $( 'input[name="design"]:radio' ).change( function() {
+
+     var radioval = $(this).val();
+     $("#design").attr('src', 'images/'+radioval+'.png');
+
+  });
+
+  $( 'input[name="car"]:radio' ).change( function() {
+
+     var radioval = $(this).val();
+     $("#design").attr('src', 'images/'+radioval+'.png');
+
+  });
+
+  $( 'input[name="hosi"]:radio' ).change( function() {
+
+   var radioval = $(this).val();
+   $("#design").attr('src', 'images/'+radioval+'.png');
+
+  });
+
+  $( 'input[name="heart"]:radio' ).change( function() {
+
+   var radioval = $(this).val();
+   $("#design").attr('src', 'images/'+radioval+'.png');
+
+  });
+
+  $( 'input[name="clover"]:radio' ).change( function() {
+
+   var radioval = $(this).val();
+   $("#design").attr('src', 'images/'+radioval+'.png');
+
+  });
+
+  $( 'input[name="hana"]:radio' ).change( function() {
+
+   var radioval = $(this).val();
+   $("#design").attr('src', 'images/'+radioval+'.png');
+
   });
